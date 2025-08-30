105cbbe742e3ee2e98fac273d02eed8d23d5b062
caiohxp <caiocesar.ccomp@gmail.com>
2025-08-30 05:39:53 -0300

    fix: texto da página inicial

--- conect.php
+++ conect.php
@@ -1,5 +1,14 @@
-<?
-$mysqli = new mysqli('127.0.0.1','ruyguer_persona','ca62la63','ruyguer_gueruyrra');
-$mysqli->set_charset("latin1");
-if (mysqli_connect_errno($mysqli)){echo "Failed to connect to MySQL: " . mysqli_connect_error();}
+<?php
+$mysqli = new mysqli('127.0.0.1', 'ruyguer_persona', 'ca62la63', 'ruyguer_gueruyrra');
+
+// Força charset correto
+if (!$mysqli->set_charset("latin1")) {
+    echo "Erro ao definir charset: " . $mysqli->error;
+}
+
+// Verifica erro de conexão
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
 ?>
--- index.php
+++ index.php
@@ -16,11 +16,11 @@
 <section class="conteudo">
 	
 <article class="centraliza">
-<div class="blocos" id="img1"><img class="foto" id="foto1" src="images/ruy-gabo.jpg" alt="Ruy com Gabriel GarcÌa M·rquez, Cuba, na 1™ metade dos anos 80" title="Ruy com Gabriel GarcÌa M·rquez, Cuba, na 1™ metade dos anos 80" /></div>
-<div class="blocos" id="img2"><img class="foto" id="foto2" src="images/ruy_guerra-chico_buarque.jpg" title="Ruy com Chico Buarque no Rio de Janeiro, inÌcio dos anos 70" alt="Ruy com Chico Buarque no Rio de Janeiro, inÌcio dos anos 70" /></div>
+<div class="blocos" id="img1"><img class="foto" id="foto1" src="images/ruy-gabo.jpg" alt="Ruy com Gabriel GarcÔøΩa MÔøΩrquez, Cuba, na 1ÔøΩ metade dos anos 80" title="Ruy com Gabriel GarcÔøΩa MÔøΩrquez, Cuba, na 1ÔøΩ metade dos anos 80" /></div>
+<div class="blocos" id="img2"><img class="foto" id="foto2" src="images/ruy_guerra-chico_buarque.jpg" title="Ruy com Chico Buarque no Rio de Janeiro, inÔøΩcio dos anos 70" alt="Ruy com Chico Buarque no Rio de Janeiro, inÔøΩcio dos anos 70" /></div>
 <div class="blocos" id="img3"><img class="foto" id="foto3" src="images/deco2.jpg" alt="Ruy estudante em Paris, 1954" title="Ruy estudante em Paris, 1954" /></div>
 <div class="blocos" id="img4"><img class="foto" id="foto4" src="images/deco1.jpg" alt="Enquadrando o horizonte, deserto do Atacama (Chile, 2014)" title="Enquadrando o horizonte, deserto do Atacama (Chile, 2014)" /></div>
-<div class="blocos" id="img5"><img class="foto" id="foto5" src="images/cinema_novo.jpg" alt="Ruy e Paulo Cesar Saraceni em reuni„o do Cinema Novo para a TV francesa (RJ - 1966)" title="Ruy e Paulo Cesar Saraceni em reuni„o do Cinema Novo para a TV francesa (RJ - 1966)" /></div>
+<div class="blocos" id="img5"><img class="foto" id="foto5" src="images/cinema_novo.jpg" alt="Ruy e Paulo Cesar Saraceni em reuniÔøΩo do Cinema Novo para a TV francesa (RJ - 1966)" title="Ruy e Paulo Cesar Saraceni em reuniÔøΩo do Cinema Novo para a TV francesa (RJ - 1966)" /></div>
 <div class="botoes">
 	<img id="click1" src="images/botao1bp.gif" alt="1" title="1" />
 	<img id="click2" src="images/botao2ap.gif" alt="2" title="2" />
@@ -34,20 +34,27 @@
 <article class="nasceu">
 	<h1 class="letreiro">RUY GUERRA</h1>
 	<p class="ruyguerra">
-		Ruy Guerra nasceu a <span class="vermelho">22/08/1931 em MoÁambique,</span> colÙnia africana portuguesa ‡s margens do Oceano Õndico. Desde jovem foi um <span class="vermelho">apaixonado pela palavra e pela imagem.</span> Escreveu poesias, contos, criticas cinematogr·ficas para a imprensa da capital LourenÁo Marques; assÌduo frequentador das matinÍs de cinema, filmou document·rio sobre o cotidiano dos trabalhadores das docas desse porto importante da ¡frica Austral.
-		<br /><br />Aos vinte anos foi para <span class="vermelho">Paris onde se formou cineasta.</span> Desembarcou no Rio de Janeiro em  julho de 1958; fez do <span class="vermelho">Brasil seu paÌs de adoÁ„o.</span> Seus dois primeiros filmes ñ Os Cafajestes (1962) e Os Fuzis (1964, Urso de Prata de Berlim) ñ logo o tornaram conhecido no cen·rio cinematogr·fico brasileiro e mundial. Foi o primeiro cineasta do grupo do Cinema Novo a filmar fora do Brasil (FranÁa, em 1968).
-		<br /><br />No Rio de Janeiro dos anos 1960/1970 foi <span class="vermelho">parceiro letrista de m˙sicos jovens</span> que se projetavam, entre outros Edu Lobo, Francis Hime, Milton Nascimento, Chico Buarque.  Com o ˙ltimo escreveu e realizou o musical Calabar: o elogio da traiÁ„o, proibido pela censura da ditadura militar  em 1973. Ainda de Chico filmou o musical A ”pera do Malandro em 1985 e em 2000  o livro Estorvo. Foi o realizador que mais levou ‡ tela trabalhos de Gabriel GarcÌa M·rquez, de quem ficou amigo desde 1972.
-		<br /><br />Andou meio mundo atr·s de sua paix„o por filmar; em meados de 1990 voltou a se fixar no Rio de Janeiro, onde nasceram e vivem seus 3 filhos e 3 netos. <br />
-		<br class="all" /><br />
-	</p>
+    Ruy Guerra nasceu a <span class="vermelho">22/08/1931 em Mo√ßambique,</span> col√¥nia africana portuguesa √†s margens do Oceano √çndico. Desde jovem foi um <span class="vermelho">apaixonado pela palavra e pela imagem.</span> Escreveu poesias, contos, cr√≠ticas cinematogr√°ficas para a imprensa da capital Louren√ßo Marques; ass√≠duo frequentador das matin√™s de cinema, filmou document√°rio sobre o cotidiano dos trabalhadores das docas desse porto importante da √Åfrica Austral.
+    <br /><br />Aos vinte anos foi para <span class="vermelho">Paris onde se formou cineasta.</span> Desembarcou no Rio de Janeiro em julho de 1958; fez do <span class="vermelho">Brasil seu pa√≠s de ado√ß√£o.</span> Seus dois primeiros filmes ‚Äì Os Cafajestes (1962) e Os Fuzis (1964, Urso de Prata de Berlim) ‚Äì logo o tornaram conhecido no cen√°rio cinematogr√°fico brasileiro e mundial. Foi o primeiro cineasta do grupo do Cinema Novo a filmar fora do Brasil (Fran√ßa, em 1968).
+    <br /><br />No Rio de Janeiro dos anos 1960/1970 foi <span class="vermelho">parceiro letrista de m√∫sicos jovens</span> que se projetavam, entre outros Edu Lobo, Francis Hime, Milton Nascimento e Chico Buarque. Com o √∫ltimo escreveu e realizou o musical <i>Calabar: o elogio da trai√ß√£o</i>, proibido pela censura da ditadura militar em 1973. Ainda com Chico, filmou o musical <i>A √ìpera do Malandro</i> em 1985 e, em 2000, a adapta√ß√£o do livro <i>Estorvo</i>. Foi o realizador que mais levou √† tela trabalhos de Gabriel Garc√≠a M√°rquez, de quem ficou amigo desde 1972.
+    <br /><br />Andou meio mundo atr√°s de sua paix√£o por filmar; em meados de 1990 voltou a se fixar no Rio de Janeiro, onde nasceram e vivem seus tr√™s filhos e tr√™s netos. <br />
+    <br class="all" /><br />
+</p>
+
 		
 <?php
 include 'conect.php';
-$sql = $mysqli->query("select count(*) from noticia where validade>now()");
-$row = $sql->fetch_array(MYSQLI_NUM);
-if ($row[0] > 0){
-	//echo "<h2 class=\"noticia\"><a href=\"noticias.php\">NOTÕCIAS</a></h2>";
-	echo "<p><a class=\"noticia2\" href=\"noticias.php\">NOTÕCIAS</a></p>";
+
+// Usa a vari√°vel $mysqli vinda do conect.php
+$sql = $mysqli->query("SELECT COUNT(*) FROM noticia WHERE validade > NOW()");
+
+if ($sql) {
+    $row = $sql->fetch_array(MYSQLI_NUM);
+    if ($row[0] > 0) {
+        echo '<p><a class="noticia2" href="noticias.php">NOT√çCIAS</a></p>';
+    }
+} else {
+    echo "Erro na consulta: " . $mysqli->error;
 }
 ?>
 </article>
